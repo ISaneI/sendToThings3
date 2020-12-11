7e5385af492c0c9be0691be742425bef2fe6830b
ISaneI <mhaidacher97@gmail.com>
2020-12-11 16:54:55 +0100

    icon hotkey and tray
    starts in tray, open and closes with ctrl alt .

--- sendToThings3/Form1.cs
+++ sendToThings3/Form1.cs
@@ -12,9 +12,30 @@ namespace sendToThings3
 {
     public partial class Form1 : Form
     {
+        KeyboardHook hook = new KeyboardHook();
+
         public Form1()
         {
             InitializeComponent();
+
+            // register the event that is fired after the key press.
+            hook.KeyPressed +=
+                new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
+            // register the control + alt + F12 combination as hot key.
+            hook.RegisterHotKey(sendToThings3.ModifierKeys.Control | sendToThings3.ModifierKeys.Alt,
+                Keys.OemPeriod);
+        }
+
+        void hook_KeyPressed(object sender, KeyPressedEventArgs e)
+        {
+            // show the keys pressed in a label.
+            if (this.Visible)
+            {
+                this.Hide();
+                return;
+            }
+
+            this.Show();
         }
     }
 }
---
+++ sendToThings3/TrayContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using sendToThings3.Properties;
+
+namespace sendToThings3
+{
+    class TrayContext : ApplicationContext
+    {
+        private Form1 _form1 = new Form1();
+
+        private NotifyIcon trayIcon;
+
+        public TrayContext()
+        {
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon()
+            {
+                Icon = Resources.icon,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                    new MenuItem("New", New),
+                    new MenuItem("Exit", Exit)
+                }),
+                Visible = true
+            };
+
+
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+
+        void New(object sender, EventArgs e)
+        {
+            _form1.Show();
+        }
+    }
+}
